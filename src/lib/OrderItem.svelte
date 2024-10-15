a198d04556554e2f57ee208d9bbde72e7d084ef2
LilleAila <olai.solsvik@gmail.com>
2024-10-15 17:56:27 +0200

    order display

---
+++ src/lib/OrderItem.svelte
@@ -0,0 +1,1 @@
+<span class="p-2 bg-gray-200 rounded"><slot /></span>
---
+++ src/routes/display/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+  import OrderItem from "$lib/OrderItem.svelte";
+</script>
+
+<div class = "grid grid-cols-1 md:grid-cols-2 gap-4 h-screen">
+  <div class="p-4 h-full flex flex-col border-b-2 md:border-b-0 md:border-r-2 border-black">
+    <h2 class="mb-4 text-3xl font-bold">Straks ferdig...</h2>
+    <div class="flex flex-wrap gap-2">
+      <OrderItem>123</OrderItem>
+      <OrderItem>456</OrderItem>
+      <OrderItem>789</OrderItem>
+    </div>
+  </div>
+  <div class="p-4 h-full flex flex-col">
+    <h2 class="mb-4 text-3xl text-green-700 font-bold">Kom og hent!</h2>
+    <div class="flex flex-wrap gap-2">
+      <OrderItem>123</OrderItem>
+      <OrderItem>456</OrderItem>
+      <OrderItem>789</OrderItem>
+    </div>
+  </div>
+</div>
