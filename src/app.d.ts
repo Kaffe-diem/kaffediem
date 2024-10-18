08b394f479b925b5d0ec933f11cb39e8f4160c54
LilleAila <olai.solsvik@gmail.com>
2024-10-18 18:07:08 +0200

    removed unused values in Locals

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,16 +1,9 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
-
-import PocketBase from "pocketbase";
-
 declare global {
   namespace App {
     // interface Error {}
-    interface Locals {
-      pb: PocketBase;
-      id: string;
-      email: string;
-    }
+    // interface Locals {}
     // interface PageData {}
     // interface PageState {}
     // interface Platform {}
