d5597ee9d9f0527c832c63633c7bd37dd00119f0
IldenH <IldenH.1@proton.me>
2024-10-20 13:38:05 +0200

    DaisyUI: kaffediem theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,20 @@ export default {
   },
 
   daisyui: {
-    themes: ["light"],
-    darkTheme: "light"
+    themes: [
+      {
+        // Modified DaisyUI "forest" theme
+        // Example themes: https://github.com/saadeghi/daisyui/blob/56313736a3eba4e12d24285caea4e1c3dbf229c1/src/theming/themes.js
+        kaffeDiem: {
+          primary: "#1eb854",
+          secondary: "#1DB88E",
+          accent: "#1DB8AB",
+          neutral: "#19362D",
+          "base-100": "#ffffff"
+        }
+      }
+    ],
+    darkTheme: "kaffeDiem"
   },
 
   plugins: [DaisyUI]
