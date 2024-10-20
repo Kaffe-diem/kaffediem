6ebaa7b6beed5024fdf4ce5c333146d2a5d0dcad
LilleAila <olai.solsvik@gmail.com>
2024-10-20 14:48:23 +0200

    daisyUI divider in display

--- src/routes/display/+page.svelte
+++ src/routes/display/+page.svelte
@@ -6,11 +6,14 @@
   $: orders = [new Order(123), new Order(456), new Order(789, State.Complete)];
 </script>
 
-  <div class="h-full grid grid-cols-1 gap-4 md:grid-cols-2">
-    <div class="flex h-full flex-col border-b-2 border-neutral p-4 md:border-b-0 md:border-r-2">
+  <div class="h-full grid grid-cols-1 gap-4 md:grid-cols-[1fr_auto_1fr]">
+    <div class="flex h-full flex-col p-4">
       <h2 class="mb-3 text-4xl text-neutral font-bold md:mb-6 md:text-center md:text-4xl">Straks ferdig...</h2>
       <OrderList {orders} filter={State.Production} />
     </div>
+
+    <div class="divider md:divider-horizontal"></div>
+
     <div class="flex h-full flex-col p-4">
       <h2 class="mb-3 text-4xl font-bold text-primary md:mb-6 md:text-center md:text-4xl">
         Kom og hent!
