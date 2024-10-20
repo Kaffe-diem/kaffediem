20b52ce3acdbef0ba9a02a34a4d903df55d2e1c9
LilleAila <olai.solsvik@gmail.com>
2024-10-20 17:53:49 +0200

    add front page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,15 +11,47 @@
   }));
 </script>
 
-<img src={Logo} alt="Kaffe diem logo" class="w-full max-w-[400px] md:max-w-[300px] md:float-left m-4 mx-auto" />
+<img src={Logo} alt="Kaffe diem logo" class="w-full max-w-[400px] md:max-w-[300px] md:pr-5 md:float-left m-4 mx-auto" />
 
 <h1 class="text-4xl">Kaffe diem</h1>
 
-Vi lager veldig god kaffe!
+<p>
+  Den beste kaffeen på amalie skram!
+</p>
+
+<div class="inline-block text-center m-2">
+  <div class="rating rating-lg rating-half">
+    {#each Array(10) as _, n}
+      <input type="radio" class="mask mask-star-2 {n % 2 == 0 ? 'mask-half-1' : 'mask-half-2'} bg-primary" checked={n == 8} disabled />
+    {/each}
+  </div>
+  <br />
+  <span class="text-sm">Fra 1729 omtaler</span>
+</div>
+
+<!-- Jump below the image -->
+<br class="clear-both" />
+
+<div class="card bg-base-200 w-full md:max-w-lg shadow">
+  <div class="card-body">
+    <h2 class="card-title">Meningen med livet</h2>
+    <div class="rating rating-half block">
+      {#each Array(10) as _, n}
+        <input type="radio" class="mask mask-star-2 {n % 2 == 0 ? 'mask-half-1' : 'mask-half-2'} bg-primary" checked={n == 9} disabled />
+      {/each}
+    </div>
+    <p>Dette er den beste kaffeen jeg noen gang har smakt!</p>
+    <p class="mt-2 italic">- Navn Navnesen</p>
+  </div>
+</div>
 
 <br />
 
-<a href="/drinks" class="link-primary">Se menyen vår</a>
+<p>
+  <a href="/drinks" class="link-primary">Se menyen vår</a>
+</p>
+
+<br />
 
 {#if isAuthenticated}
   <a href="/drinks" class="btn">Trykk her for å bestille</a>
