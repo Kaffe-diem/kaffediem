a67b360aac4465775454ca9957ab937b9e828f6a
LilleAila <olai.solsvik@gmail.com>
2024-10-20 17:02:13 +0200

    started writing front page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,29 @@
 <script>
   import { auth } from "$lib/stores/authStore";
   import { onMount } from "svelte";
+  import Logo from "$lib/assets/logo.png";
+  import AuthButton from "$lib/AuthButton.svelte";
 
   let isAuthenticated = false;
-  let user = null;
 
   onMount(auth.subscribe((value) => {
     isAuthenticated = value.isAuthenticated;
-    user = value.user;
   }));
 </script>
 
+<img src={Logo} alt="Kaffe diem logo" class="w-full max-w-[400px] md:max-w-[300px] md:float-left m-4 mx-auto" />
+
+<h1 class="text-4xl">Kaffe diem</h1>
+
+Vi lager veldig god kaffe!
+
+<br />
+
+<a href="/drinks" class="link-primary">Se menyen vår</a>
+
 {#if isAuthenticated}
-  <p>Hei {user?.name}, du er logget inn!</p>
+  <a href="/drinks" class="btn">Trykk her for å bestille</a>
 {:else}
-  <p>Hei, du er ikke logget inn :(</p>
+  <AuthButton class="btn m-2">Logg inn for å bestille</AuthButton>
+  <p class="text-xs">Ved å opprette en bruker samtykker du til våre <a href="/tos" class="link-primary">vilkår for bruk</a></p>
 {/if}
---
+++ src/routes/tos/+page.svelte
@@ -0,0 +1,52 @@
+<p>
+  Ved å opprette en bruker samtykker du til å dele:
+</p>
+
+<ul class="list-disc pl-4">
+  <li>E-postaddressen din</li>
+  <li>Navnet ditt</li>
+  <li>Profilbildet ditt</li>
+</ul>
+
+<p>
+  Du kan til enhver tid trekke tilbake samtykken din ved å kontakte oss på <a href="mailto:kaffediem@asvg.no" class="link-primary">kaffediem@asvg.no</a>
+</p>
+
+<br />
+
+<p>
+  Vi bruker informasjonskapsler for å holde deg pålogget. Du kan administrere innstillingene for informasjonskapsler i nettleseren din.
+</p>
+
+<br />
+
+<p>
+  I tillegg vil vi samle inn følgende data om brukere:
+</p>
+
+<ul class="list-disc pl-4">
+  <li>Hva du kjøper</li>
+  <li>Hvor mye kaffe du kjøper</li>
+  <li>Når du kjøper kaffe</li>
+</ul>
+
+<p>
+  Vi samler inn denne dataen for å forbedre brukeropplevelsen og tilpasse tjenestene.
+  <br />Dataen din vil kun bli delt med tredjeparter dersom det er nødvendig for skolearbeid, og vil aldri bli solgt til andre.
+</p>
+
+<br />
+
+<p>
+  Dataene dine vil bli lagret så lenge det er nødvendig for formålene nevnt ovenfor.
+</p>
+
+<p>
+  Du har rett til å anskaffe, rette på og fjerne dataen din når som helst ved å kontakte oss på <a href="mailto:kaffediem@asvg.no" class="link-primary">kaffediem@asvg.no</a>
+</p>
+
+<br />
+
+<p class="text-sm">
+  Sist oppdatert <i>20. Oktober 2024</i>
+</p>
