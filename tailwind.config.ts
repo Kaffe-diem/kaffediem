d7fdbe9f5cd050405a06a44773318d0bf69ab052
IldenH <IldenH.1@proton.me>
2024-10-20 13:45:36 +0200

    Add base-200 and base-300 to theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,11 +14,13 @@ export default {
         // Modified DaisyUI "forest" theme
         // Example themes: https://github.com/saadeghi/daisyui/blob/56313736a3eba4e12d24285caea4e1c3dbf229c1/src/theming/themes.js
         kaffeDiem: {
-          primary: "#1eb854",
+          primary: "#1EB854",
           secondary: "#1DB88E",
           accent: "#1DB8AB",
           neutral: "#19362D",
-          "base-100": "#ffffff"
+          "base-100": "#FFFFFF",
+          "base-200": "#F2F2F2",
+          "base-300": "#E5E6E6"
         }
       }
     ],
