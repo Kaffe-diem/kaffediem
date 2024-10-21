4be468afa6fadd4b27d17b1571a60eff5e4be803
didriksn <102746612+didriksn@users.noreply.github.com>
2024-10-21 14:37:46 +0200

    Update src/routes/display/+layout.svelte
    Co-authored-by: IldenH <84644748+IldenH@users.noreply.github.com>

--- src/routes/display/+layout.svelte
+++ src/routes/display/+layout.svelte
@@ -5,7 +5,6 @@
 <main class="h-screen relative py-4 w-11/12 mx-auto">
     <slot />
 </main>
-
   <div class="absolute bottom-0 left-0 hidden md:flex">
     <a href="/">
       <img class="h-48 w-48" src={QR} alt="QR code"/>
