606f4aedc942353e36f205f94c1f572441f2af50
Martin Kleiven <m.klei@live.no>
2024-10-21 21:51:26 +0200

    Implement /account, first user specific page

---
+++ src/lib/Drink.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  export let favorite: any;
+  import { pb } from "$lib/stores/authStore";
+</script>
+
+<div class="card bg-base-100 shadow-md transition-all duration-100 hover:scale-[101%] hover:shadow-lg ">
+  <figure>
+    <img
+      class=""
+      src={pb.files.getUrl(favorite.expand.drink, favorite.expand.drink.image)}
+      alt={favorite.expand.drink.name}
+    />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">{favorite.expand.drink.name}</h2>
+    <p>Price: ${favorite.expand.drink.price}</p>
+    <p>Serving Size: {favorite.serving_size}</p>
+    <p>Extras: {favorite.extras}</p>
+    <p>Milk: {favorite.milk}</p>
+    <p>Flavor: {favorite.flavor}</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Bestill igjen</button>
+      <button class="btn btn-tertiary btn-ghost">Slett</button>
+    </div>
+  </div>
+</div>
--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -1,1 +1,28 @@
-Denne routen er protectet og kan kun vise hvis man er logget in. Den skal kanskje vise data om din egen bruker i fremtiden
+<script lang="ts">
+    import { PUBLIC_PB_HOST } from "$env/static/public";
+import { pb } from "$lib/stores/authStore";
+import { onMount } from "svelte";
+import Drink from "$lib/Drink.svelte";
+
+let favorites: any[] = [];
+
+onMount(async () => {
+  const favoriteIds = pb.authStore.model?.favorites;
+  // all
+  favorites = await pb.collection("order_drink").getFullList(100, {
+    expand: "drink",
+  });
+  // or single attached to user
+  favorites = await pb.collection("order_drink").getOne(favoriteIds[0]);
+});
+
+</script>
+
+<h1 class="my-16 text-4xl font-bold text-teal-700 italic">
+  {pb.authStore.model?.name.split(" ")[0].toUpperCase()} ELSKER
+</h1>
+<div class="grid grid-cols-3 gap-16">
+  {#each favorites as favorite}
+    <Drink {favorite} />
+  {/each}
+</div>
