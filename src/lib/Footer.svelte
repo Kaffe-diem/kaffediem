1d9abde1b5d05268cf279d38e8f3f03743721a87
LilleAila <olai.solsvik@gmail.com>
2024-10-22 13:39:15 +0200

    add footer

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,24 @@
+<script>
+  // import Logo from "$lib/assets/logo.png";
+  const year = new Date().getFullYear()
+</script>
+
+<footer class="footer bg-base-200 text-base-content p-10 mt-10">
+  <aside>
+    <!-- <img src={Logo} alt="logo" /> -->
+    [Insert logo her]
+    <p>
+      Copyright © {year} Kaffe diem
+      <br />
+      Den beste kaffeen på Amalie Skram
+    </p>
+  </aside>
+  <nav>
+    <h6 class="footer-title">Vilkår og personvern</h6>
+    <a href="/tos" class="link link-hover">Vilkår for bruk</a>
+  </nav>
+  <nav>
+    <h6 class="footer-title">Få hjelp</h6>
+    <a href="mailto:kaffediem@asvg.no" class="link link-hover">Kontakt oss</a>
+  </nav>
+</footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,16 +2,23 @@
   import "../app.css";
   import { page } from '$app/stores';
   import Nav from "$lib/Nav.svelte";
+  import Footer from "$lib/Footer.svelte";
 </script>
 
-<!-- Display-kiosk-siden skal ikke ha noen navbar; den skal kun vise bestillinger -->
-{#if $page.url.pathname.includes("display")}
-  <slot />
-{:else}
-  <div class="app mx-auto w-11/12 md:w-9/12">
-    <Nav />
-    <main class="mt-16">
-      <slot />
-    </main>
-  </div>
-{/if}
+<div class="min-h-screen flex flex-col">
+<div class="flex-grow">
+  <!-- Display-kiosk-siden skal ikke ha noen navbar; den skal kun vise bestillinger -->
+  {#if $page.url.pathname.includes("display")}
+    <slot />
+  {:else}
+    <div class="app mx-auto w-11/12 md:w-9/12">
+      <Nav />
+      <main class="mt-16">
+        <slot />
+      </main>
+    </div>
+  {/if}
+</div>
+
+<Footer />
+</div>
