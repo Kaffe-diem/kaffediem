a27698d7cd46ea75e0f9da5d63250adbfa3a498e
Martin Kleiven <m.klei@live.no>
2024-10-22 21:24:19 +0200

    Fix favorites, implement history

--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -1,27 +1,46 @@
 <script lang="ts">
-  import { PUBLIC_PB_HOST } from "$env/static/public";
   import { pb } from "$lib/stores/authStore";
   import { onMount } from "svelte";
-  import Drink from "$lib/ExpandedDrink.svelte";
+  import ExpandedDrink from "$lib/ExpandedDrink.svelte";
 
-  let favorites: any[] = [];
-
+  let favorites: any[];
+  let previous_orders_drinks: any[];
   onMount(async () => {
-    const favoriteIds = pb.authStore.model?.favorites;
-    // all
-    favorites = await pb.collection("order_drink").getFullList(100, {
-      expand: "drink"
-    });
-    // or single attached to user
-    favorites = await pb.collection("order_drink").getOne(favoriteIds[0]);
+    const userId = pb.authStore.model?.id;
+    if (userId) {
+      const user = await pb.collection("users").getOne(userId, {
+        expand: "favorites,favorites.drink"
+      });
+      favorites = user.expand?.favorites || [];
+
+      const orders = await pb.collection("orders").getList(1, 10, {
+        filter: `customer = '${userId}'`,
+        expand: "drinks,drinks.drink"
+      });
+      previous_orders_drinks = orders.items.flatMap((order) => order.expand?.drinks || []);
+      console.log(previous_orders_drinks);
+    }
   });
 </script>
 
-<h1 class="my-16 text-4xl font-bold italic text-teal-700">
-  {pb.authStore.model?.name.split(" ")[0].toUpperCase()} ELSKER
-</h1>
-<div class="grid grid-cols-3 gap-16">
-  {#each favorites as favorite}
-    <Drink {favorite} />
-  {/each}
+<div class="items-between flex">
+  <section>
+    <h1 class="my-16 text-4xl font-bold italic text-teal-700">
+      {pb.authStore.model?.name.split(" ")[0].toUpperCase()} ELSKER
+    </h1>
+    <div class="grid grid-cols-3 gap-16">
+      {#each favorites || [] as drink}
+        <ExpandedDrink {drink} />
+      {/each}
+    </div>
+  </section>
+
+  <section>
+    <h1 class="my-16 text-4xl font-bold italic text-teal-700">SISTE BESTILLINGER</h1>
+    <div class="grid grid-cols-3 gap-16">
+      {#each previous_orders_drinks ?? [] as drink}
+        <ExpandedDrink {drink} />
+      {/each}
+    </div>
+  </section>
 </div>
