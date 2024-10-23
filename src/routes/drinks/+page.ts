26d24f26dfc3669b87d3c6977ff33c93a86e8a3f
LilleAila <olai.solsvik@gmail.com>
2024-10-23 20:04:54 +0200

    fix fetch function used to fetch

--- src/routes/drinks/+page.ts
+++ src/routes/drinks/+page.ts
@@ -1,7 +1,8 @@
 import { pb } from "$lib/stores/authStore";
 
-export const load = async () => ({
+export const load = async ({ fetch }) => ({
   drinks: await pb.collection("drinks").getFullList({
-    sort: "-created"
+    sort: "-created",
+    fetch
   })
 });
