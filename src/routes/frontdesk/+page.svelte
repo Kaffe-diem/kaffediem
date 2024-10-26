f6b6aec33282492459ea05cad229f67fbd320cc1
IldenH <IldenH.1@proton.me>
2024-10-26 22:05:07 +0200

    Copy /drinks to /frontdesk and modify

---
+++ src/routes/frontdesk/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import { pb } from "$lib/stores/authStore";
+  let groupedDrinks: Record<string, any[]>;
+
+  export let data = { drinks: [] };
+  groupedDrinks = Object.groupBy(data.drinks, ({ kind }) => kind);
+</script>
+
+<div class="flex flex-col gap-16">
+  <section>
+    <h1 class="text-2xl font-bold italic text-red-700">Varm drikke</h1>
+    <ul class="grid grid-cols-1 gap-16 md:grid-cols-8">
+      {#each groupedDrinks?.hot || [] as drink}
+        <input type="radio" aria-label={drink.name} class="btn" />
+      {/each}
+    </ul>
+  </section>
+
+  <section>
+    <h1 class="text-2xl font-bold italic text-sky-700">Kald drikke</h1>
+    <ul class="grid grid-cols-1 gap-16 md:grid-cols-8">
+      {#each groupedDrinks?.cold || [] as drink}
+        <input type="radio" aria-label={drink.name} class="btn" />
+      {/each}
+    </ul>
+  </section>
+
+  <section>
+    <h1 class="text-2xl font-bold italic text-orange-700">Sesongens spesial 🎃</h1>
+    <ul class="grid grid-cols-1 gap-16 md:grid-cols-8">
+      {#each groupedDrinks?.special || [] as drink}
+        <input type="radio" aria-label={drink.name} class="btn" />
+      {/each}
+    </ul>
+  </section>
+</div>
---
+++ src/routes/frontdesk/+page.ts
@@ -0,0 +1,8 @@
+import { pb } from "$lib/stores/authStore";
+
+export const load = async ({ fetch }) => ({
+  drinks: await pb.collection("drinks").getFullList({
+    sort: "-created",
+    fetch
+  })
+});
