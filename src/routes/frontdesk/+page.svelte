30a4b462166e3c931e637be9c02e88c0460a96aa
IldenH <IldenH.1@proton.me>
2024-10-26 22:30:00 +0200

    Fix radio buttons frontdesk

--- src/routes/frontdesk/+page.svelte
+++ src/routes/frontdesk/+page.svelte
@@ -11,7 +11,7 @@
     <h1 class="text-2xl font-bold italic text-red-700">Varm drikke</h1>
     <ul class="grid grid-cols-1 gap-16 md:grid-cols-8">
       {#each groupedDrinks?.hot || [] as drink}
-        <input type="radio" aria-label={drink.name} class="btn" />
+        <input type="radio" name="drink" aria-label={drink.name} class="btn" />
       {/each}
     </ul>
   </section>
@@ -20,7 +20,7 @@
     <h1 class="text-2xl font-bold italic text-sky-700">Kald drikke</h1>
     <ul class="grid grid-cols-1 gap-16 md:grid-cols-8">
       {#each groupedDrinks?.cold || [] as drink}
-        <input type="radio" aria-label={drink.name} class="btn" />
+        <input type="radio" name="drink" aria-label={drink.name} class="btn" />
       {/each}
     </ul>
   </section>
@@ -29,7 +29,7 @@
     <h1 class="text-2xl font-bold italic text-orange-700">Sesongens spesial 🎃</h1>
     <ul class="grid grid-cols-1 gap-16 md:grid-cols-8">
       {#each groupedDrinks?.special || [] as drink}
-        <input type="radio" aria-label={drink.name} class="btn" />
+        <input type="radio" name="drink" aria-label={drink.name} class="btn" />
       {/each}
     </ul>
   </section>
