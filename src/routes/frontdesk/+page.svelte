3cdf20c9ac4fb859fd0908369b26e790f4d26461
LilleAila <olai.solsvik@gmail.com>
2024-10-27 17:57:42 +0100

    format code

--- src/routes/frontdesk/+page.svelte
+++ src/routes/frontdesk/+page.svelte
@@ -15,14 +15,22 @@
 <div class="flex flex-col gap-8">
   {#each data.categories as category}
     <section>
-      <h1 class="text-2xl mb-4 font-bold text-primary">{category.name}</h1>
-      <ul class="grid gap-6 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+      <h1 class="mb-4 text-2xl font-bold text-primary">{category.name}</h1>
+      <ul class="grid grid-cols-2 gap-6 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
         {#each category.expand.drinks_via_category as drink}
           <label>
-            <input type="radio" name="drink" class="hidden peer" value={drink} bind:group={selectedDrink} />
-            <div class="w-full h-24 btn border-4 peer-checked:border-accent relative flex justify-center items-center flex-col">
+            <input
+              type="radio"
+              name="drink"
+              class="peer hidden"
+              value={drink}
+              bind:group={selectedDrink}
+            />
+            <div
+              class="btn relative flex h-24 w-full flex-col items-center justify-center border-4 peer-checked:border-accent"
+            >
               <span class="font-bold">{drink.name}</span>
-              <span class="font-normal absolute bottom-2 right-2">{drink.price},-</span>
+              <span class="absolute bottom-2 right-2 font-normal">{drink.price},-</span>
             </div>
           </label>
         {/each}
