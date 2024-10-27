ae0e0efbb65faa357cedc8b6ab32471f8cda2675
LilleAila <olai.solsvik@gmail.com>
2024-10-27 17:41:46 +0100

    variable in constants for routes to hide footer and navbar

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,5 @@
 export const restrictedRoutes = ["/account", "/status"];
+export const hideNavbar = ["/display"];
+export const hideFooter = ["/display", "/frontdesk"];
+
 export const stateColors = ["neutral", "yellow-600", "primary"];
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,12 +3,14 @@
   import { page } from "$app/stores";
   import Nav from "$components/Nav.svelte";
   import Footer from "$components/Footer.svelte";
+
+  import { hideNavbar, hideFooter } from "$lib/constants.ts";
 </script>
 
 <div class="flex min-h-screen flex-col">
   <div class="flex-grow">
     <!-- Display-kiosk-siden skal ikke ha noen navbar; den skal kun vise bestillinger -->
-    {#if $page.url.pathname.includes("display")}
+    {#if hideNavbar.includes($page.url.pathname)}
       <slot />
     {:else}
       <div class="app mx-auto w-11/12 md:w-9/12">
@@ -20,5 +22,7 @@
     {/if}
   </div>
 
-  <Footer />
+  {#if !hideFooter.includes($page.url.pathname)}
+    <Footer />
+  {/if}
 </div>
