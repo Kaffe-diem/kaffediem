740c65a506f6127873d1573b39a697718ae4c53f
Martin Kleiven <m.klei@live.no>
2024-11-01 11:05:50 +0100

    Subtle exit

--- src/routes/frontdesk/+layout.svelte
+++ src/routes/frontdesk/+layout.svelte
@@ -1,7 +1,9 @@
+<script>
+  import EscapeFullViewButton from "$lib/components/EscapeFullViewButton.svelte";
+  
+</script>
 <main class="relative mx-auto h-screen w-11/12 py-4">
   <slot />
 </main>
 
-<div class="absolute bottom-0 right-0 m-8 hidden md:flex">
-  <a href="/" class="btn btn-secondary">Tilbake</a>
-</div>
+<EscapeFullViewButton />
