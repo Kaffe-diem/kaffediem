c43244b9164cc707e70f0ad1b44533dc3c2e44e1
IldenH <IldenH.1@proton.me>
2024-11-04 10:21:02 +0100

    Revert "Subtle exit"
    This reverts commit 740c65a506f6127873d1573b39a697718ae4c53f.

--- src/routes/frontdesk/+layout.svelte
+++ src/routes/frontdesk/+layout.svelte
@@ -1,9 +1,7 @@
-<script>
-  import EscapeFullViewButton from "$lib/components/EscapeFullViewButton.svelte";
-  
-</script>
 <main class="relative mx-auto h-screen w-11/12 py-4">
   <slot />
 </main>
 
-<EscapeFullViewButton />
+<div class="absolute bottom-0 right-0 m-8 hidden md:flex">
+  <a href="/" class="btn btn-secondary">Tilbake</a>
+</div>
