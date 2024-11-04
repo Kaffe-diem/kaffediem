9ffff2a34b73c7a6afe89510cdae7a255d1420dd
IldenH <IldenH.1@proton.me>
2024-11-04 10:20:46 +0100

    Revert "lint"
    This reverts commit 64df86bf0773f888f09fcea22ce3832790486c21.

--- src/routes/frontdesk/+layout.svelte
+++ src/routes/frontdesk/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
   import EscapeFullViewButton from "$lib/components/EscapeFullViewButton.svelte";
+  
 </script>
-
 <main class="relative mx-auto h-screen w-11/12 py-4">
   <slot />
 </main>
