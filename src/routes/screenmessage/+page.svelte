235627afc745eb08969d104b8c09fac2c7c6366f
IldenH <IldenH.1@proton.me>
2024-11-09 18:51:47 +0100

    Screenmessage: open radio works

--- src/routes/screenmessage/+page.svelte
+++ src/routes/screenmessage/+page.svelte
@@ -1,14 +1,23 @@
 <script lang="ts">
   import { pb } from "$lib/stores/authStore";
 
-  export let data: { screenMessages };
-  data = data.screenMessages;
+  export let data;
+  let screenMessages = data.screenMessages;
+  let activeMessage = data.activeMessage[0];
+
+  let open = !activeMessage.isVisible;
+  async function updateActiveMessage() {
+    await pb.collection("activeMessage").update(activeMessage.id, {
+      isVisible: !open
+    });
+  }
 
   let selectedMessage;
 
   async function updateScreenMessages() {
-    for (let message of data) {
-      await pb.collection("screen_message").update(message.id, {
+    updateActiveMessage();
+    for (let message of screenMessages) {
+      await pb.collection("screenMessages").update(message.id, {
         title: message.title,
         subtext: message.subtext,
         isVisible: message.id == selectedMessage.id
@@ -19,7 +28,7 @@
 
 <form>
   <ul class="list-none">
-    {#each data as message}
+    {#each screenMessages as message}
       <div class="flex">
         <div class="form-control">
           <label class="label cursor-pointer">
@@ -29,7 +38,10 @@
               class="radio mr-2 mt-4"
               value={message}
               checked={message.isVisible}
-              on:change={() => (selectedMessage = message)}
+              on:change={() => {
+                selectedMessage = message;
+                open = false;
+              }}
             />
 
             <li>
@@ -54,7 +66,13 @@
       </div>
     {/each}
 
-    <input type="radio" name="selected" class="radio mr-2 mt-4" />
+    <input
+      type="radio"
+      name="selected"
+      class="radio mr-2 mt-4"
+      checked={open}
+      on:change={() => (open = true)}
+    />
     <span class="text-lg">Åpent!</span>
   </ul>
 
--- src/routes/screenmessage/+page.ts
+++ src/routes/screenmessage/+page.ts
@@ -1,5 +1,6 @@
 import { pb } from "$lib/stores/authStore";
 
 export const load = async ({ fetch }) => ({
-  screenMessages: await pb.collection("screen_message").getFullList({ fetch })
+  screenMessages: await pb.collection("screenMessages").getFullList({ fetch }),
+  activeMessage: await pb.collection("activeMessage").getFullList({ fetch })
 });
