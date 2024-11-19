aa13b2885d67ec6d738443651d778efcb5014292
Martin Kleiven <m.klei@live.no>
2024-11-19 18:09:43 +0100

    use expanded prop

--- src/routes/admin/OrderList.svelte
+++ src/routes/admin/OrderList.svelte
@@ -17,10 +17,15 @@
         <li>
           <!-- maybe on hover/click (if click, swipe to delete) we can show the contents? -->
           <button
-            class="btn mt-4 w-full text-xl font-normal md:btn-lg md:text-3xl"
+            class="btn mt-4 flex w-full space-x-2 text-xl font-normal md:btn-lg md:text-3xl"
             onclick={() => orders.setState(order.id, onclick)}
           >
             {index + 100}
+            {#each order.expand.drinks as drink}
+              <span class="badge badge-primary">
+                {drink.expand.drink.name}
+              </span>
+            {/each}
           </button>
         </li>
       {/if}
