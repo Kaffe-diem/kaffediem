9c22ea5e902f1d184bee9fbde3ea736d0120190b
LilleAila <olai.solsvik@gmail.com>
2024-11-24 13:10:31 +0100

    display: fix text centering

--- src/routes/display/+layout.svelte
+++ src/routes/display/+layout.svelte
@@ -11,7 +11,7 @@
 </script>
 
 {#if $activeMessage.visible}
-  <div class="flex h-screen flex-col items-center justify-center">
+  <div class="flex h-screen flex-col items-center justify-center text-center">
     <span class="p-2 text-7xl font-bold md:text-9xl">{$activeMessage.message.title}</span>
     <span class="p-2 text-4xl md:text-6xl">{$activeMessage.message.subtext}</span>
   </div>
