677631d46be430bd17c9eebc8e7f9fdd1855f423
LilleAila <olai.solsvik@gmail.com>
2024-11-24 18:51:09 +0100

    OrderList: remove separators between rows

--- src/routes/admin/OrderList.svelte
+++ src/routes/admin/OrderList.svelte
@@ -20,7 +20,7 @@
     <tbody>
       {#each $orders as order, index}
         {#if show.includes(order.state)}
-          <tr class="hover" onclick={() => orders.updateState(order.id, onclick)}>
+          <tr class="hover border-none" onclick={() => orders.updateState(order.id, onclick)}>
             <td>{index + 100}</td>
             <td>
               {#each order.drinks as drink}
