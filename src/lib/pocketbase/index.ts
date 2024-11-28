1991f96775f0cd3f3dad22b185dfb0a24511db87
LilleAila <olai.solsvik@gmail.com>
2024-11-28 15:07:58 +0100

    pocketbase: use TypedPocketBase

--- src/lib/pocketbase/index.ts
+++ src/lib/pocketbase/index.ts
@@ -1,7 +1,8 @@
 import PocketBase from "pocketbase";
 import { PUBLIC_PB_HOST } from "$env/static/public";
+import type { TypedPocketBase } from "$lib/pb-types";
 
-const pb = new PocketBase(PUBLIC_PB_HOST);
+const pb = new PocketBase(PUBLIC_PB_HOST) as TypedPocketBase;
 pb.autoCancellation(false);
 
 export default pb;
