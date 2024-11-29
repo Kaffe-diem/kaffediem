633961dde62addc90e8546022238d2b72a598270
IldenH <IldenH.1@proton.me>
2024-11-29 19:53:43 +0100

    Nav: drawer instead of dropdown

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -26,27 +26,34 @@
   ];
 </script>
 
-<div class="navbar bg-base-100">
-  <div class="flex-1">
-    <a href="/" class="btn btn-ghost text-xl">Kaffe Diem</a>
+<div class="drawer drawer-end z-[1]">
+  <input id="drawer" type="checkbox" class="drawer-toggle" />
+  <div class="drawer-content flex flex-col">
+    <div class="navbar w-full">
+      <div class="flex-1">
+        <a href="/" class="btn btn-ghost text-xl">Kaffe Diem</a>
+      </div>
+      <div class="flex-none lg:hidden">
+        <label for="drawer" aria-label="open sidebar" class="btn btn-square btn-ghost">
+          <MenuIcon />
+        </label>
+      </div>
+      <div class="hidden flex-none lg:block">
+        <ul class="menu menu-horizontal">
+          <NavItems
+            {navItems}
+            isAuthenticated={$auth.isAuthenticated}
+            isAdmin={$auth.user?.is_admin}
+            class="menu menu-horizontal px-1"
+          />
+        </ul>
+      </div>
+    </div>
   </div>
-  <details class="dropdown dropdown-end lg:hidden">
-    <summary tabindex="0" class="btn btn-ghost">
-      <MenuIcon />
-    </summary>
-    <NavItems
-      {navItems}
-      isAuthenticated={$auth.isAuthenticated}
-      isAdmin={$auth.user?.is_admin}
-      class="dropdown-content z-[1] mt-3 w-52 rounded-box bg-base-100 p-2 shadow"
-    />
-  </details>
-  <div class="hidden flex-none lg:flex">
-    <NavItems
-      {navItems}
-      isAuthenticated={$auth.isAuthenticated}
-      isAdmin={$auth.user?.is_admin}
-      class="menu menu-horizontal px-1"
-    />
+  <div class="drawer-side">
+    <label for="drawer" aria-label="close sidebar" class="drawer-overlay"></label>
+    <ul class="menu min-h-full w-80 bg-base-100 p-4">
+      <NavItems {navItems} isAuthenticated={$auth.isAuthenticated} isAdmin={$auth.user?.is_admin} />
+    </ul>
   </div>
 </div>
