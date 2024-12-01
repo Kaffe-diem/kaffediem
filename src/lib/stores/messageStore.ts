3ce21caf27a2ce5e28e85fb4899d72cad22abc71
Martin Kleiven <m.klei@live.no>
2024-12-01 21:32:49 +0100

    get activemessagestore working as a normal store

--- src/lib/stores/messageStore.ts
+++ src/lib/stores/messageStore.ts
@@ -1,12 +1,10 @@
 import createPbStore from "$stores/pbStore";
 import pb, {
   Collections,
-  type ActiveMessageRecord,
   type ActiveMessageResponse,
   type DisplayMessagesResponse,
   type RecordIdString
 } from "$lib/pocketbase";
-import { writable } from "svelte/store";
 
 import eventsource from "eventsource";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -31,37 +29,14 @@ export const messages = {
   }
 };
 
-function createActiveMessageStore() {
-  // Initialize with dummy non-visible message
-  const { subscribe, set } = writable<ActiveMessageRecord>();
-
-  (async () => {
-    const initialData = await pb.collection(Collections.ActiveMessage).getFullList({
-      expand: "message"
-    });
-
-    set(initialData[0]);
-
-    pb.collection(Collections.ActiveMessage).subscribe(
-      "*",
-      (event: { record: ActiveMessageRecord }) => {
-        set(event.record);
-      },
-      {
-        expand: "message"
-      }
-    );
-  })();
-
-  return subscribe;
-}
-
 export const activeMessage = {
-  subscribe: createActiveMessageStore(),
-  update: async (message: ActiveMessageResponse) => {
-    await pb.collection(Collections.ActiveMessage).update(message.id, {
-      message: message.message.id,
-      isVisible: message.isVisible
+  subscribe: createPbStore(Collections.ActiveMessage, {
+    expand: "message"
+  }),
+  update: async (current: ActiveMessageResponse) => {
+    await pb.collection(Collections.ActiveMessage).update(current.id, {
+      message: current?.expand.message.id,
+      isVisible: current.isVisible
     });
   }
 };
