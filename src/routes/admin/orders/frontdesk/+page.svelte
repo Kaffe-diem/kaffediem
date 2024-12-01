9229ff9f8a6486cfb19848d437189774be83cb81
Martin Kleiven <m.klei@live.no>
2024-12-01 22:04:35 +0100

    type cart, save some errors

--- src/routes/admin/orders/frontdesk/+page.svelte
+++ src/routes/admin/orders/frontdesk/+page.svelte
@@ -2,9 +2,9 @@
   import OrderList from "../OrderList.svelte";
   import ItemSelection from "./ItemSelection.svelte";
   import Cart from "./Cart.svelte";
-  import { OrdersStateOptions } from "$lib/pocketbase";
+  import { OrdersStateOptions, type DrinksResponse } from "$lib/pocketbase";
 
-  let selectedItem = $state();
+  let selectedItem = $state<DrinksResponse | undefined>();
 </script>
 
 <div class="grid h-full grid-cols-[2fr,auto,1fr,auto,1fr] grid-rows-[100%] gap-2">
