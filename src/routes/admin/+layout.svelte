8c97a3206618fcbf3ea447671f1a7afbe659a88f
DumbAugust <carlaugust.sponberg@gmail.com>
2024-12-04 18:24:01 +0100

    Format

--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -1,16 +1,16 @@
 <script>
-  import { goto } from '$app/navigation';
+  import { goto } from "$app/navigation";
   let { children } = $props();
 
   const redirectToPrevious = () => {
-    const length = $state(window.location.pathname.split('/').length);
+    const length = $state(window.location.pathname.split("/").length);
     let href = "";
     for (let i = 0; i < length - 2; i++) {
-      href += '.';
+      href += ".";
     }
-    href += '/';
+    href += "/";
     goto(href);
-  }
+  };
 </script>
 
 <main class="relative mx-auto w-11/12 py-4">
