754b8e022d78a21ecf20601f1c9a6633c2117e7b
LilleAila <olai.solsvik@gmail.com>
2025-03-30 17:49:21 +0200

    menu: simplify reading data

--- src/routes/admin/menu/edit/[slug]/+page.svelte
+++ src/routes/admin/menu/edit/[slug]/+page.svelte
@@ -1,29 +1,20 @@
 <script lang="ts">
-  import { onMount } from "svelte";
   import type { PageProps } from "./$types";
   import { items, categories } from "$stores/menuStore";
-  import { get, derived } from "svelte/store";
 
   let { data }: PageProps = $props();
   const id = data.id;
-
-  // const item = $items.find((item) => item.id === id);
-  // const category = $categories.find((category) => category.id == item.category)
-
-  const item = derived(items, ($items) => $items.find((item) => item.id === id));
-
-  const item_category = derived(categories, ($categories) =>
-    $categories.find((category) => category.id == $item.category)
-  );
 
   let itemName: string = $state();
   let itemPrice: number = $state();
   let itemCategory: string = $state();
   $effect(() => {
-    if ($item) {
-      itemName = $item.name;
-      itemPrice = $item.price;
-      itemCategory = $item.category;
+    const item = $items.find((item) => item.id === id);
+    const category = $categories.find((category) => category.id == item.category);
+    if (item) {
+      itemName = item.name;
+      itemPrice = item.price;
+      itemCategory = category.id;
     }
   });
 
@@ -33,34 +24,32 @@
 </script>
 
 <div class="grid w-full grid-cols-2 gap-2">
-  {#if $item}
-    <div class="col-span-2">
-      <fieldset class="fieldset">
-        <legend class="fieldset-legend">Navn</legend>
-        <input type="text" class="input w-full" bind:value={itemName} />
-      </fieldset>
-    </div>
-    <div>
-      <fieldset class="fieldset">
-        <legend class="fieldset-legend">Pris</legend>
-        <input type="number" class="input w-full" bind:value={itemPrice} />
-      </fieldset>
-    </div>
-    <div>
-      <fieldset class="fieldset">
-        <legend class="fieldset-legend">Kategori</legend>
-        <select class="select" bind:value={itemCategory}>
-          {#if $item_category}
-            {#each $categories as category}
-              <option value={category.id} selected={category.id == $item_category.id}
-                >{category.name}</option
-              >
-            {/each}
-          {/if}
-        </select>
-      </fieldset>
-    </div>
-  {/if}
+  <div class="col-span-2">
+    <fieldset class="fieldset">
+      <legend class="fieldset-legend">Navn</legend>
+      <input type="text" class="input w-full" bind:value={itemName} />
+    </fieldset>
+  </div>
+  <div>
+    <fieldset class="fieldset">
+      <legend class="fieldset-legend">Pris</legend>
+      <input type="number" class="input w-full" bind:value={itemPrice} />
+    </fieldset>
+  </div>
+  <div>
+    <fieldset class="fieldset">
+      <legend class="fieldset-legend">Kategori</legend>
+      <select class="select" bind:value={itemCategory}>
+        {#if itemCategory}
+          {#each $categories as category}
+            <option value={category.id} selected={category.id == itemCategory.id}
+              >{category.name}</option
+            >
+          {/each}
+        {/if}
+      </select>
+    </fieldset>
+  </div>
   <div class="col-span-2">
     <button class="btn btn-primary w-full" onclick={updateItem}>Lagre</button>
   </div>
