92581875aa20544239461b78ca0c8c1b1197ed3a
LilleAila <olai.solsvik@gmail.com>
2025-03-30 18:54:26 +0200

    admin: simplify back button

--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -1,16 +1,5 @@
 <script>
-  import { goto } from "$app/navigation";
   let { children } = $props();
-
-  const redirectToPrevious = () => {
-    const length = $state(window.location.pathname.split("/").length);
-    let href = "";
-    for (let i = 0; i < length - 2; i++) {
-      href += ".";
-    }
-    href += "/";
-    goto(href);
-  };
 </script>
 
 <main class="relative mx-auto w-11/12 py-4">
@@ -18,5 +7,5 @@
 </main>
 
 <div class="fixed right-0 bottom-0 m-8 hidden md:flex">
-  <button onclick={redirectToPrevious} class="btn btn-secondary">Tilbake</button>
+  <a href="/admin" class="btn btn-secondary">Tilbake</a>
 </div>
