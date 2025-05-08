acd2c9f594e0a484fd7ecf26a4a76f4fb9bc9ef7
LilleAila <olai.solsvik@gmail.com>
2025-05-08 17:39:13 +0200

    display/OrderList: flex -> grid

--- src/routes/display/OrderList.svelte
+++ src/routes/display/OrderList.svelte
@@ -15,12 +15,10 @@
   <h2 class="mb-3 text-4xl font-bold md:mb-6 md:text-center lg:text-6xl {className}">
     {label}
   </h2>
-  <div class="flex flex-wrap gap-2">
+  <div class="grid max-h-full grid-cols-4 gap-2 overflow-y-scroll">
     {#each $orders as order, index}
       {#if show.includes(order?.state)}
-        <div
-          class="bg-base-200 text-neutral px-4 py-4 text-3xl font-normal md:px-6 md:py-6 md:text-4xl lg:px-12 lg:py-6 lg:text-5xl"
-        >
+        <div class="bg-base-200 text-neutral px-4 py-4 text-center text-4xl">
           {index + 100}
         </div>
       {/if}
