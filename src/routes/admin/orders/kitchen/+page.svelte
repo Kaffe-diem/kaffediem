9a8752184b9ac63be3cfb319dc8896061f2749c9
IldenH <84644748+IldenH@users.noreply.github.com>
2025-06-08 13:14:07 +0200

    orders: oops button (#127)
    * orders: oops button init
    
    * toasts: orderNumber - 1
    
    * orders: oops button action history
    
    * actionHistory: type hint
    
    * orderStore: enum action history
    
    * oops: simplify to only state
    
    * oops: make kitchen not scroll screen
    
    * kitchen: max-h-screen -> h-screen

--- src/routes/admin/orders/kitchen/+page.svelte
+++ src/routes/admin/orders/kitchen/+page.svelte
@@ -1,14 +1,19 @@
 <script lang="ts">
   import { OrderStateOptions } from "$lib/pocketbase";
   import OrderList from "../OrderList.svelte";
+  import orders from "$stores/orderStore";
 
   const { received, production, completed } = OrderStateOptions;
 </script>
 
-<div class="grid h-full grid-cols-[1fr_auto_1fr] grid-rows-[100%] gap-4">
-  <OrderList show={[received]} onclick={production} label="Mottatt" />
+<div class="grid h-screen grid-rows-[1fr_auto] p-4">
+  <div class="grid max-h-screen grid-cols-[1fr_auto_1fr] gap-4 overflow-y-auto">
+    <OrderList show={[received]} onclick={production} label="Mottatt" />
 
-  <div class="divider divider-horizontal"></div>
+    <div class="divider divider-horizontal"></div>
 
-  <OrderList show={[production]} onclick={completed} label="Lages" />
+    <OrderList show={[production]} onclick={completed} label="Lages" />
+  </div>
+
+  <button class="btn btn-lg mt-4" onclick={() => orders.undoLastAction()}>oops</button>
 </div>
