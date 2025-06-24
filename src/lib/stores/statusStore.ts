c235daceeec677e25b977652c2d7c2dc3c6e0a50
Martin Kleiven <m.klei@live.no>
2025-06-25 00:16:20 +0200

    also statusStore

--- src/lib/stores/statusStore.ts
+++ src/lib/stores/statusStore.ts
@@ -2,6 +2,7 @@ import { createGenericPbStore } from "$stores/pbStore";
 import pb, { Collections, type MessageResponse } from "$lib/pocketbase";
 import { Message, Status } from "$lib/types";
 import { writable } from "svelte/store";
+import { browser } from '$app/environment';
 
 import eventsource from "eventsource";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -12,43 +13,45 @@ export const messages = createGenericPbStore(Collections.Message, Message);
 function createStatusStore() {
   const { subscribe, set, update } = writable(Status.baseValue);
 
-  (async () => {
-    // Only use the first record. Assumes that PB already has this and only this record.
-    const initialActiveMessage = await pb.collection(Collections.Status).getFirstListItem("");
-    const initialMessages: MessageResponse[] = await pb
-      .collection(Collections.Message)
-      .getFullList();
+  if (browser) {
+    (async () => {
+      // Only use the first record. Assumes that PB already has this and only this record.
+      const initialActiveMessage = await pb.collection(Collections.Status).getFirstListItem("");
+      const initialMessages: MessageResponse[] = await pb
+        .collection(Collections.Message)
+        .getFullList();
 
-    const initialData = Status.fromPb(initialActiveMessage, initialMessages.map(Message.fromPb));
-    set(initialData);
+      const initialData = Status.fromPb(initialActiveMessage, initialMessages.map(Message.fromPb));
+      set(initialData);
 
-    pb.collection(Collections.Status).subscribe("*", async (event) => {
-      update((state) => {
-        return Status.fromPb(event.record, state.messages);
+      pb.collection(Collections.Status).subscribe("*", async (event) => {
+        update((state) => {
+          return Status.fromPb(event.record, state.messages);
+        });
       });
-    });
 
-    pb.collection(Collections.Message).subscribe("*", (event) => {
-      update((state) => {
-        const itemIndex = state.messages.findIndex((item) => item.id == event.record.id);
-        const item = Message.fromPb(event.record);
+      pb.collection(Collections.Message).subscribe("*", (event) => {
+        update((state) => {
+          const itemIndex = state.messages.findIndex((item) => item.id == event.record.id);
+          const item = Message.fromPb(event.record);
 
-        switch (event.action) {
-          case "create":
-            state.messages.push(item);
-            break;
-          case "update":
-            if (itemIndex !== -1) state.messages[itemIndex] = item;
-            break;
-          case "delete":
-            if (itemIndex !== -1) state.messages.splice(itemIndex, 1);
-            break;
-        }
+          switch (event.action) {
+            case "create":
+              state.messages.push(item);
+              break;
+            case "update":
+              if (itemIndex !== -1) state.messages[itemIndex] = item;
+              break;
+            case "delete":
+              if (itemIndex !== -1) state.messages.splice(itemIndex, 1);
+              break;
+          }
 
-        return state;
+          return state;
+        });
       });
-    });
-  })();
+    })();
+  }
 
   return subscribe;
 }
