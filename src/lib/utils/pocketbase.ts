204131de25fcaf2ca6d499f23c005038eecdb27e
Martin Kleiven <m.klei@live.no>
2025-06-25 15:44:48 +0200

    Preserve localhost over 0.0.0.0

--- src/lib/utils/pocketbase.ts
+++ src/lib/utils/pocketbase.ts
@@ -1,20 +1,24 @@
 import { browser } from "$app/environment";
 
 export const getPocketBasePath = (): string => {
-	if (!browser) {
+  if (!browser) {
     return "http://pb:8081";
-	}
+  }
 
-	const { hostname } = window.location;
+  const { hostname } = window.location;
 
-	if (hostname === "kaffediem.okpl.us") {
-		return "https://kaffebase.okpl.us";
-	}
+  if (hostname === "kaffediem.okpl.us") {
+    return "https://kaffebase.okpl.us";
+  }
 
-	if (hostname.endsWith(".kaffediem.okpl.us")) {
-		const subdomain = hostname.split(".")[0];
-		return `https://${subdomain}.kaffebase.okpl.us`;
-	}
+  if (hostname.endsWith(".kaffediem.okpl.us")) {
+    const subdomain = hostname.split(".")[0];
+    return `https://${subdomain}.kaffebase.okpl.us`;
+  }
+
+  if (hostname === "localhost" || hostname === "127.0.0.1") {
+    return "http://127.0.0.1:8081";
+  }
 
   return "http://0.0.0.0:8081";
 };
