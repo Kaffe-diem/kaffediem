e4df9a761df33283f36442e545d9d3a0cfcdf10d
Martin Kleiven <m.klei@live.no>
2025-06-25 18:00:39 +0200

    add error stack to logs

--- scripts/sync-db.js
+++ scripts/sync-db.js
@@ -43,7 +43,7 @@ const main = async () => {
       `✅ Backup ${latestRemoteBackup.key} downloaded and extracted successfully. Local version updated.`
     );
   } catch (error) {
-    console.error("❌ Error:", error.message);
+    console.error("❌ Error:", error.message, error.stack);
     process.exit(1);
   }
 };
