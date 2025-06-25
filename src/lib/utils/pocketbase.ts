08e96135f6991936a02cc9dfcab5aa8e46be9b6a
Martin Kleiven <m.klei@live.no>
2025-06-25 12:30:08 +0200

    Modify PB to rely on discovery

--- src/lib/utils/pocketbase.ts
+++ src/lib/utils/pocketbase.ts
@@ -2,8 +2,20 @@ import { PUBLIC_PB_HOST } from "$env/static/public";
 import { browser } from "$app/environment";
 
 export const getPocketBasePath = (): string => {
-  const pbUrl = !browser
-    ? PUBLIC_PB_HOST.replace(/:\/\/(localhost|0\.0\.0\.0|127\.0\.0\.1):/, "://pb:")
-    : PUBLIC_PB_HOST;
-  return pbUrl;
+	if (!browser) {
+		return "http://pb:8081";
+	}
+
+	const { hostname } = window.location;
+
+	if (hostname === "kaffediem.okpl.us") {
+		return "https://kaffebase.okpl.us";
+	}
+
+	if (hostname.endsWith(".kaffediem.okpl.us")) {
+		const subdomain = hostname.split(".")[0];
+		return `https://${subdomain}.kaffebase.okpl.us`;
+	}
+
+	return PUBLIC_PB_HOST;
 };
