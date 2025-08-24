3cc9c90abbdcc6ee4cf7c81e171173d02c98fc35
IldenH <IldenH.1@proton.me>
2025-08-24 14:05:07 +0200

    frontdesk: slightly different grid-cols

--- src/routes/admin/orders/frontdesk/+page.svelte
+++ src/routes/admin/orders/frontdesk/+page.svelte
@@ -10,7 +10,7 @@
   let selectedItem = $state<Item | undefined>();
 </script>
 
-<div class="grid h-full grid-cols-[3fr_auto_2fr_auto_1fr] grid-rows-[100%] gap-0 p-4">
+<div class="grid h-full grid-cols-[5fr_auto_5fr_auto_2fr] grid-rows-[100%] gap-0 p-4">
   <ItemSelection bind:selectedItem />
 
   <div class="divider divider-horizontal m-0 p-0"></div>
