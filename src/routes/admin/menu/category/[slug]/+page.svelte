4d856f963d23850b45075beb2825585cd2aff23d
IldenH <IldenH.1@proton.me>
2025-09-14 17:58:06 +0200

    menu: category update validCustomizationKeys

--- src/routes/admin/menu/category/[slug]/+page.svelte
+++ src/routes/admin/menu/category/[slug]/+page.svelte
@@ -12,19 +12,21 @@
   let { data }: PageProps = $props();
   const id = data.id;
   const create = id == "new";
+  const category = $categories.find((category) => category.id === id);
 
   let categoryName: string | undefined = $state();
   let categorySort: number | undefined = $state();
   let categoryEnabled: boolean = $state(true);
+  let categoryValidCustomizationKeys: string[] | undefined = $state();
 
   let exists: boolean = $state(false);
 
   $effect(() => {
-    const category = $categories.find((category) => category.id === id);
     if (category) {
       categoryName = category.name;
       categorySort = category.sortOrder;
       categoryEnabled = category.enabled;
+      categoryValidCustomizationKeys = category.validCustomizationKeys;
 
       exists = true;
     }
@@ -32,9 +34,25 @@
 
   function updateCategory() {
     if (create) {
-      categories.create(new Category(id, categoryName!, categorySort!, categoryEnabled));
+      categories.create(
+        new Category(
+          id,
+          categoryName!,
+          categorySort!,
+          categoryEnabled,
+          categoryValidCustomizationKeys!
+        )
+      );
     } else {
-      categories.update(new Category(id, categoryName!, categorySort!, categoryEnabled));
+      categories.update(
+        new Category(
+          id,
+          categoryName!,
+          categorySort!,
+          categoryEnabled,
+          categoryValidCustomizationKeys!
+        )
+      );
     }
     goto("/admin/menu");
   }
@@ -70,11 +88,29 @@
     <fieldset class="fieldset">
       <legend class="fieldset-legend text-xl">Tilpasninger</legend>
       <ul class="list-none">
-        {#each $customizationKeys as customization}
+        {#each $customizationKeys as customizationKey}
           <li class="my-6">
             <label class="grid grid-cols-[1fr_1fr]">
-              <span class="text-xl">{customization.name}</span>
-              <input type="checkbox" class="checkbox checkbox-xl" />
+              <span class="text-xl">{customizationKey.name}</span>
+              <input
+                type="checkbox"
+                checked={categoryValidCustomizationKeys?.includes(customizationKey.id)}
+                class="checkbox checkbox-xl"
+                onchange={(event) => {
+                  if (event.currentTarget.checked) {
+                    if (!categoryValidCustomizationKeys?.includes(customizationKey.id)) {
+                      categoryValidCustomizationKeys = [
+                        ...(categoryValidCustomizationKeys ?? []),
+                        customizationKey.id
+                      ];
+                    }
+                  } else {
+                    categoryValidCustomizationKeys = categoryValidCustomizationKeys?.filter(
+                      (id) => id !== customizationKey.id
+                    );
+                  }
+                }}
+              />
             </label>
           </li>
         {/each}
