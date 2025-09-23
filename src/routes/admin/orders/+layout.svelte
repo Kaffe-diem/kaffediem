b317b8d4053d571ff372e1d43d736b982ac0ded1
IldenH <84644748+IldenH@users.noreply.github.com>
2025-09-23 15:25:25 +0200

    message: option to show message while open (#206)
    * message: option to show message while open
    
    * message: style as requested by kaffe diem
    
    * message: showMessage button
    
    * lint
    
    * message: update status on message contents change
    
    ---------
    
    Co-authored-by: LilleAila <olai.solsvik@gmail.com>

--- src/routes/admin/orders/+layout.svelte
+++ src/routes/admin/orders/+layout.svelte
@@ -11,15 +11,20 @@
   let { children }: Props = $props();
 </script>
 
-{#if $status.online}
+{#if $status.open}
+  <main class="relative mx-auto h-screen w-full xl:w-11/12">
+    {@render children?.()}
+  </main>
+{:else}
   <div class="flex h-full w-full flex-col items-center justify-center">
     <span class="p-2 text-center text-3xl font-bold md:text-6xl">Det er stengt</span>
     <button
       class="btn relative m-4 flex h-24 w-1/2 flex-col items-center justify-center text-3xl lg:text-5xl
 "
       onclick={() =>
-        status.update(new Status($status.id, $status.message, $status.messages, false))}
-      >Åpne</button
+        status.update(
+          new Status($status.id, $status.message, $status.messages, false, $status.showMessage)
+        )}>Åpne</button
     >
     <a
       href={resolve("/admin/message")}
@@ -27,8 +32,4 @@
       >Eller endre status</a
     >
   </div>
-{:else}
-  <main class="relative mx-auto h-screen w-full xl:w-11/12">
-    {@render children?.()}
-  </main>
 {/if}
