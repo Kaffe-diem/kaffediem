443b1087b4deff85cde8f4da015a7333c8017110
IldenH <IldenH.1@proton.me>
2025-09-27 19:51:53 +0200

    display: bg-warning production, sorted

--- src/routes/display/OrderList.svelte
+++ src/routes/display/OrderList.svelte
@@ -9,6 +9,13 @@
   }
 
   let { show, label = "", class: className = "" }: Props = $props();
+
+  const stateOrder = {
+    [OrderStateOptions.production]: 0,
+    [OrderStateOptions.received]: 1,
+    [OrderStateOptions.completed]: 2,
+    [OrderStateOptions.dispatched]: 3
+  };
 </script>
 
 <div class="flex h-full flex-col p-4 select-none">
@@ -20,10 +27,15 @@
     </h2>
   {/if}
   <div class="grid max-h-full grid-cols-3 gap-4 overflow-x-hidden overflow-y-scroll xl:grid-cols-4">
-    {#each $orders as order (order.id)}
+    {#each $orders.sort((a, b) => {
+      const stateDiff = stateOrder[a.state] - stateOrder[b.state];
+      return stateDiff !== 0 ? stateDiff : b.dayId - a.dayId;
+    }) as order (order.id)}
       {#if show.includes(order?.state)}
         <div
-          class="{className} rounded-xl px-4 py-4 text-center text-[clamp(3rem,4vw,12rem)] leading-none font-bold"
+          class="{className} {order.state == OrderStateOptions.production
+            ? 'bg-warning'
+            : ''} rounded-xl px-4 py-4 text-center text-[clamp(3rem,4vw,12rem)] leading-none font-bold"
         >
           {order.dayId}
         </div>
