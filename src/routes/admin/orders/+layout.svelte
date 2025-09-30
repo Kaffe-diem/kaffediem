2fe3531b549ee400585dfb636ef14ed2d9a107d8
didriksn <didrik.sommersten@gmail.com>
2025-09-30 15:25:46 +0200

    style: update layout height to use dynamic viewport height

--- src/routes/admin/orders/+layout.svelte
+++ src/routes/admin/orders/+layout.svelte
@@ -12,7 +12,7 @@
 </script>
 
 {#if $status.open}
-  <main class="relative mx-auto h-screen w-full xl:w-11/12">
+  <main class="relative mx-auto h-dvh w-full xl:w-11/12">
     {@render children?.()}
   </main>
 {:else}
