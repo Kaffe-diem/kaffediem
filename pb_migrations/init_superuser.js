3f32c196635ea3b9157fc2361ed348bded79beb5
IldenH <IldenH.1@proton.me>
2025-10-01 12:35:52 +0200

    pb: init-superuser migration

--- pb_migrations/init_superuser.js
+++ pb_migrations/init_superuser.js
@@ -1,12 +1,14 @@
 migrate((app) => {
     let superusers = app.findCollectionByNameOrId("_superusers")
 
-    let record = new Record(superusers)
+    if (process.env.NODE_ENV == "development") {
+      let record = new Record(superusers)
 
-    record.set("email", process.env.PB_TEST_ADMIN_EMAIL)
-    record.set("password", process.env.PB_TEST_ADMIN_PASSWORD)
+      record.set("email", process.env.PB_TEST_ADMIN_EMAIL)
+      record.set("password", process.env.PB_TEST_ADMIN_PASSWORD)
 
-    app.save(record)
+      app.save(record)
+    }
 }, (app) => {
     try {
         let record = app.findAuthRecordByEmail("_superusers", process.env.PB_TEST_ADMIN_EMAIL)
