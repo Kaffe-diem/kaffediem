6488fe57309272c79eca74edd22bf29402f5480a
LilleAila <olai.solsvik@gmail.com>
2025-10-01 18:25:32 +0200

    display: increase message open height

--- src/routes/display/+layout.svelte
+++ src/routes/display/+layout.svelte
@@ -14,7 +14,7 @@
 {#if $status.showMessage && ($status.message.title || $status.message.subtitle)}
   <div
     class="flex {$status.open
-      ? 'h-2/5 justify-start'
+      ? 'h-1/2 justify-start'
       : 'h-screen justify-center'} bg-base-100 fixed bottom-0 z-100 w-full cursor-none flex-col items-center text-center"
   >
     <span class="p-4 text-[clamp(3.5rem,10vw,16rem)] leading-tight font-bold">
