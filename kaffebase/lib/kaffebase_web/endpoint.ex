dd3e6c9104592a109e009c63e8d759f11674335e
Martin Kleiven <m.klei@live.no>
2025-10-06 09:25:37 +0200

    regex in endpoint is unsupported

--- kaffebase/lib/kaffebase_web/endpoint.ex
+++ kaffebase/lib/kaffebase_web/endpoint.ex
@@ -56,7 +56,16 @@ defmodule KaffebaseWeb.Endpoint do
   plug Plug.Session, @session_options
 
   plug CORSPlug,
-    origin: ["http://localhost:5173"],
+    origin: [
+      # production
+      "https://kaffediem.okpl.us",
+      # internal docker names
+      "http://app:3000",
+      "http://app:5173",
+      # localhost
+      "http://localhost:5173",
+      "http://127.0.0.1:5173",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     headers: ["Authorization", "Content-Type", "Accept", "X-Requested-With"]
